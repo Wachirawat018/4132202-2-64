35f981cfea1eb360a750a628fcdef89aba183bda
Wachirawat018 <630112418018@bru.ac.th>
2021-12-18 12:16:35 +0700

    lab3

---
+++ lab3/src/components/Dom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const age = 25;
+
+const dom =(
+<>
+<h1>Hello React !!!</h1>
+<p>My name is Wachirawat,{age} years old.</p>
+</>
+);
+
+function Dom() {
+return dom;
+}
+
+export default Dom;
